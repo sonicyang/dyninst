fa79b72db0b57f14806fd29dffcd93c27c1c9d0c
Tim Haines <thaines.astro@gmail.com>
2022-04-29 17:07:03 -0500

    Change TOC behavior

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,11 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "collapse_navigation" : False,
+    "sticky_navigation" : False,
+    "display_version" : True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
